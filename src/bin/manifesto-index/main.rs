609a5ca1790bb3b96e19d74d8169278e0385d4b8
Max Countryman <maxc@me.com>
2018-04-26 17:10:16 -0700

    refactor indexer so it uses smaller functions

--- src/bin/manifesto-index/main.rs
+++ src/bin/manifesto-index/main.rs
@@ -5,10 +5,10 @@ extern crate walkdir;
 
 use std::collections::HashMap;
 use std::env;
-use std::error::Error;
 use std::fs::File;
 use std::fs;
 use std::io::Read;
+use std::path::Path;
 use std::str;
 
 use md5::Digest;
@@ -22,37 +22,45 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
-fn dir_to_manifest(dir: &str) -> Result<HashMap<String, String>, Box<Error>> {
+fn is_dir(path: &Path) -> bool {
+    fs::metadata(path)
+        .expect("Could not read file metadata.")
+        .is_dir()
+}
+
+fn read_path(path: &Path) -> Vec<u8> {
+    let mut file = File::open(path).expect("File not found.");
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).expect("Unable to read file.");
+
+    buf
+}
+
+fn manifest_from_dir(dir: &str) -> HashMap<String, String> {
     let walker = WalkDir::new(dir).into_iter();
     let mut manifest = HashMap::new();
 
     for entry in walker.filter_entry(|e| !is_hidden(e)) {
-        let entry = entry?;
+        let entry = entry.expect("No entries found.");
         let path = entry.path();
 
-        let md = fs::metadata(path).expect("Could not read file metadata.");
-        if md.is_dir() {
+        if is_dir(path) {
             continue;
         }
 
-        let path_display = path.display();
+        let buf = read_path(path);
 
-        let mut file = File::open(path).expect("File not found.");
-        let mut contents = Vec::new();
-        file.read_to_end(&mut contents)
-            .expect("Unable to read file.");
-
-        let hash_str = format!("{:x}", md5::Md5::digest(&contents));
+        let hash = format!("{:x}", md5::Md5::digest(&buf));
         let ext = path.extension().expect("Missing file extension.");
-        let ext_str = ext.to_os_string()
+        let ext = ext.to_os_string()
             .into_string()
-            .expect("Cannot convert to string.");
-        let hashed_name = [hash_str, ext_str].join(".");
+            .expect("Cannot convert extention to string.");
+        let hashed_name = [hash, ext].join(".");
 
-        manifest.insert(path_display.to_string(), hashed_name);
+        manifest.insert(path.display().to_string(), hashed_name);
     }
 
-    Ok(manifest)
+    manifest
 }
 
 fn main() {
@@ -62,8 +70,7 @@ fn main() {
     let input_dir = args.iter().nth(1).expect("Missing input directory.");
     let output = args.iter().nth(2).unwrap_or(&default_output);
 
-    let manifest = dir_to_manifest(&input_dir).expect("Could not build manifest.");
+    let manifest = manifest_from_dir(&input_dir);
     let writer = m::manifest_writer(output);
-
     serde_json::to_writer(writer, &manifest).expect("Failed to serialize manifest.");
 }
