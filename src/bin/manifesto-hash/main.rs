76d9e612cd56cc1ed7fc07ee9855c929f30f4535
Max Countryman <maxc@me.com>
2018-05-01 09:29:07 -0700

    clean up

--- src/bin/manifesto-hash/main.rs
+++ src/bin/manifesto-hash/main.rs
@@ -21,7 +21,7 @@ fn main() {
 
     for (file_name, new_name) in manifest {
         let new_file = [output.to_string(), new_name].join("");
-        fs::copy([String::from("public"), file_name].join(""), new_file)
-            .expect("Could not rename file.");
+        let file_name = [String::from("public"), file_name].join("");
+        fs::copy(file_name, new_file).expect("Could not rename file.");
     }
 }
